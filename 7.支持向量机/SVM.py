998349f3f09beb3135220a9ca49a627d8c3c309f
ruaruazero <ruarua.zero@gmail.com>
2024-10-25 23:59:56 +0800

    SVM

---
+++ 7.支持向量机/SVM.py
@@ -0,0 +1,87 @@
+import numpy as np
+
+
+class SVM:
+
+    def __init__(self) -> None:
+        self.alpha = None
+        self.Y = None
+        self.X = None
+        self.kernel = None
+        self.b = None
+        self.C = None
+        self.p = None
+        self.sigma = None
+        self.epsilon = None
+        self.max_iter = None
+
+    def load_data(self, data):
+        pass
+
+    def fit(self):
+        # 初始化
+        self._initialize()
+        k = 0
+        b = 0
+        while k < self.max_iter:
+            for i in range(len(self.X)):
+                # 选择两点进行优化
+                a_i, x_i, y_i = self.alpha[i], self.X[i], self.Y[i]
+                j = self.select_j(i)
+                a_j, x_j, y_j = self.alpha[j], self.X[j], self.Y[j]
+                # 计算核函数和期望等
+                Kii = self.kernel(x_i, x_i)
+                Kjj = self.kernel(x_j, x_j)
+                Kij = self.kernel(x_i, x_j)
+                eta = Kii + Kjj - 2 * Kij
+                Ei = self.forward(x_i) - y_i
+                Ej = self.forward(x_j) - y_j
+                # 计算上下界
+                L = np.max([0, a_j - a_i])
+                H = np.min([self.C, self.C + a_j - a_i])
+                # 计算新的a_j, a_i
+                a_j_new = a_j + y_j * (Ei - Ej) / eta
+                if a_j_new > H:
+                    a_j_new = H
+                elif a_j_new < L:
+                    a_j_new = L
+                a_i_new = a_i + y_i * y_j * (a_j - a_j_new)
+                if abs(a_j_new - a_j) < 0.00001:
+                    continue
+                index = (self.alpha > 0) * (self.alpha < self.C)
+                index = np.arange(len(self.alpha))[index]
+                if len(index) == 0:
+                    continue
+                else:
+                    index = index[0]
+                    b = self.Y[index] - self.forward(self.X[index])
+                # 更新 alpha 和 b
+                self.alpha[i] = a_i_new
+                self.alpha[j] = a_j_new
+                self.b = b
+            k += 1
+
+
+    def select_j(self, i):
+        index = np.arange(len(self.X))
+        index = index[index != i]
+        return np.random.choice(index)
+    
+    def forward(self, x):
+        value = 0
+        for j, z in enumerate(self.X):
+            value += self.alpha[j] * self.Y[j] * self.kernel(z, x)
+        return value + self.b
+
+    def _initialize(self):
+        self.alpha = np.zeros(self.X.shape[0])
+
+    def _poly_kernel(self, x, z):
+        return (x @ z + 1) ** self.p
+    
+    def _gaussian_kernel(self, x, z):
+        return np.exp(-np.linalg.norm(x - z) / (2 * self.sigma ** 2))
+    
+    @staticmethod
+    def _linear(x, z):
+        return x @ z
